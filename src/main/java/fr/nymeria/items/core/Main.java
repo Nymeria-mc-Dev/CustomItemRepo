39fcd3a6e452d81125bf9ca4ca2806b87f03bf67
SorenDeveloppement <sorenel@yahoo.com>
2022-08-08 14:22:28 +0200

    azef

--- src/main/java/fr/nymeria/items/core/Main.java
+++ src/main/java/fr/nymeria/items/core/Main.java
@@ -1,10 +1,13 @@
 package fr.nymeria.items.core;
 
+import fr.nymeria.items.customItems.ExempleItem;
 import org.bukkit.Bukkit;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+
+    public static ExempleItem EXEMPLE_ITEM = new ExempleItem();
 
     @Override
     public void onEnable() {
